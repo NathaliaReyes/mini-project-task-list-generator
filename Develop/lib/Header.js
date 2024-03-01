5ea591ceb2895588dd68967054ca119f9279b05d
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-02-29 22:00:55 -0700

    TaskListItem class added to the project

--- Develop/lib/Header.js
+++ Develop/lib/Header.js
@@ -1,5 +1,11 @@
 const Component = require("./Component.js");
 
 class Header extends Component{
-    
+    constructor() {
+        super();
+    }
+
+    render() {
+        return `<header><h1>Todo Today</h1><p>{DATE-HERE}</p></header>`
+    }
 }
---
+++ Develop/lib/TaskListItem.js
@@ -0,0 +1,7 @@
+const Component = require("./Component.js");
+
+class TaskListItem extends Component{
+    constructor() {
+        this.priority = priority;
+    }
+}
